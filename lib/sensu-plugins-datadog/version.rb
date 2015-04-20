10d02815edb1dd71bf924dd84e5bce6f4431866e
Matt Jones <mattyjones@users.noreply.github.com>
2015-04-20 14:03:55 -0400

    Update version.rb

--- lib/sensu-plugins-datadog/version.rb
+++ lib/sensu-plugins-datadog/version.rb
@@ -1,1 +1,28 @@
-2d1764e0649cd23de1b456f7ae87f0422331c0bd
+require 'json'
+
+# encoding: utf-8
+module SensuPluginsDatadog
+  # This defines the version of the gem
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 2
+
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+
+    NAME   = 'sensu-plugins-datadog'
+    BANNER = "#{NAME} v%s"
+
+    module_function
+
+    def version
+      format(BANNER, VER_STRING)
+    end
+
+    def json_version
+      {
+        'version' => VER_STRING
+      }.to_json
+    end
+  end
+end
